bb5e1cbccf4be399ab5ca3d06654dc354813eccd
NataliaColome <nmcolome@gmail.com>
2017-04-10 08:59:54 -0600

    created all the tests I could think of, started writing double_every_2nd_digit

--- lib/credit_check_recheck.rb
+++ lib/credit_check_recheck.rb
@@ -4,10 +4,12 @@ class CreditCheck
 
   def initialize(card_number)
     @card_number = card_number
+    @valid = false
   end
   
-  def double
-    @card_number.split(//)
-    
+  def double_every_2nd_digit
+    if @card_number.odd?
+      @card_number.split(//)
+    end
   end
 end
--- test/credit_check_recheck_test.rb
+++ test/credit_check_recheck_test.rb
@@ -14,9 +14,36 @@ class CreditCheckTest < Minitest::Test
   def test_if_doubles_every_other_number
     check = CreditCheck.new("79927398713")
 
-    output = check.double
+    output = check.double_every_2nd_digit
 
     assert_equal [7, 18, 9, 4, 7, 6, 9, 16, 7], output
+  end
+
+  def test_it_sums_double_digits
+    skip
+    check = CreditCheck.new("79927398713")
+
+    output = check.turn_to_single
+    
+    assert_equal [7, 9, 9, 4, 7, 6, 9, 7, 7], output
+  end
+
+  def test_it_sums_all_digits
+    skip
+    check = CreditCheck.new("79927398713")
+
+    output = check.sums
+
+    assert_equal 70, output
+  end
+
+  def test_its_a_valid_card_number
+    skip
+    check = CreditCheck.new("79927398713")
+
+    output = check.validate
+
+    assert output
   end
 end
 
