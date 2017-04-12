07210745207e9158dcdeac3f12803f23fd67c23a
NataliaColome <nmcolome@gmail.com>
2017-04-11 21:22:27 -0600

    wrote every method based on the test

--- lib/credit_check_recheck.rb
+++ lib/credit_check_recheck.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class CreditCheck
 
   attr_reader :card_number
@@ -6,10 +7,68 @@ class CreditCheck
     @card_number = card_number
     @valid = false
   end
-  
-  def double_every_2nd_digit
-    if @card_number.odd?
-      @card_number.split(//)
+
+  def validate
+    double
+    turn_to_single
+    sums
+    valid?
+  end
+
+  def double
+    if card_number.to_i.odd?
+      double_every_2nd_digit_if_odd
+    else
+      double_every_2nd_digit_if_even
+    end
+  end
+
+  def double_every_2nd_digit_if_odd
+      numbers = card_number.split(//)
+      doubled_n = []
+      (0...numbers.length).each do |index|
+        if index % 2 == 0
+          doubled_n << numbers[index].to_i
+        else
+          doubled_n << numbers[index].to_i * 2
+        end
+      end
+      doubled_n
+  end
+
+  def double_every_2nd_digit_if_even
+      numbers = card_number.split(//)
+      doubled_n = []
+      (0...numbers.length).each do |index|
+        if index % 2 == 1
+          doubled_n << numbers[index].to_i
+        else
+          doubled_n << numbers[index].to_i * 2
+        end
+      end
+      doubled_n
+  end
+
+  def turn_to_single
+    double.map do |number|
+      # binding.pry
+      if number > 9
+        number - 9 
+      else
+        number
+      end
+    end
+  end
+
+  def sums
+    turn_to_single.inject(0) {|sum, num| sum + num}
+  end
+
+  def valid?
+    if sums % 10 == 0
+      @valid = true
+    else
+      @valid
     end
   end
 end
--- test/credit_check_recheck_test.rb
+++ test/credit_check_recheck_test.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/credit_check_recheck'
@@ -14,22 +14,20 @@ class CreditCheckTest < Minitest::Test
   def test_if_doubles_every_other_number
     check = CreditCheck.new("79927398713")
 
-    output = check.double_every_2nd_digit
+    output = check.double
 
-    assert_equal [7, 18, 9, 4, 7, 6, 9, 16, 7], output
+    assert_equal [7, 18, 9, 4, 7, 6, 9, 16, 7, 2, 3], output
   end
 
   def test_it_sums_double_digits
-    skip
     check = CreditCheck.new("79927398713")
 
     output = check.turn_to_single
     
-    assert_equal [7, 9, 9, 4, 7, 6, 9, 7, 7], output
+    assert_equal [7, 9, 9, 4, 7, 6, 9, 7, 7, 2, 3], output
   end
 
   def test_it_sums_all_digits
-    skip
     check = CreditCheck.new("79927398713")
 
     output = check.sums
@@ -38,12 +36,35 @@ class CreditCheckTest < Minitest::Test
   end
 
   def test_its_a_valid_card_number
-    skip
     check = CreditCheck.new("79927398713")
 
-    output = check.validate
+    output = check.valid?
 
     assert output
+  end
+
+  def test_number_cards_are_valid
+    check_v0 = CreditCheck.new("5541808923795240")
+    check_v1 = CreditCheck.new("4024007136512380")
+    check_v2 = CreditCheck.new("6011797668867828")
+    check_inv0 = CreditCheck.new("5541801923795240")
+    check_inv1 = CreditCheck.new("4024007106512380")
+    check_inv2 = CreditCheck.new("6011797668868728")
+
+    assert check_v0.valid?
+    assert check_v1.valid?
+    assert check_v2.valid?
+    refute check_inv0.valid?
+    refute check_inv0.valid?
+    refute check_inv0.valid?
+  end
+
+  def test_if_it_works_for_American_Express
+    check_ae_v = CreditCheck.new("342804633855673")
+    check_ae_inv = CreditCheck.new("342801633855673")
+
+    assert check_ae_v.valid?
+    refute check_ae_inv.valid?
   end
 end
 
